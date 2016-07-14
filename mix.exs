7526251b6b4aeb1ae8a3c77b39067086a20051be
Zatvobor <a.zatvornitskiy@gmail.com>
2016-07-14 19:11:56 +0300

    Add :spell as dependency

--- mix.exs
+++ mix.exs
@@ -2,26 +2,34 @@ defmodule Krasukha.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :krasukha,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     package: package,
-     deps: deps()]
+    [
+      app: :krasukha,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      package: package,
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: [:logger],
-     mod: {Krasukha, []}]
+    [
+      applications: [:logger, :spell],
+      mod: {Krasukha, []}
+    ]
   end
 
   defp deps do
-    []
+    [
+      {:spell, github: "zatvobor/spell", branch: "master"}
+    ]
   end
 
   defp package do
-    [maintainers: ["Aleksey Zatvobor"], licenses: ["MIT"], links: %{"GitHub" => github, "Contributors" => contributors, "Issues" => issues} ]
+    [
+      maintainers: ["Aleksey Zatvobor"], licenses: ["MIT"], links: %{"GitHub" => github, "Contributors" => contributors, "Issues" => issues}
+    ]
   end
 
   defp github, do: "https://github.com/Zatvobor/krasukha"
