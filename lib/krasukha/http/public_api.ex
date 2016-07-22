5517b4429f3708698e5f335a420c4949fa5ec9fc
Zatvobor <a.zatvornitskiy@gmail.com>
2016-07-22 16:11:41 +0300

    normalizeReturnOrderBook/1

--- lib/krasukha/http/public_api.ex
+++ lib/krasukha/http/public_api.ex
@@ -9,6 +9,14 @@ defmodule Krasukha.HTTP.PublicAPI do
   @doc false
   def returnOrderBook(params \\ [currencyPair: "BTC_NXT", depth: 1]) do
     url = HTTP.url("returnOrderBook", params, uri)
-    HTTP.get(url)
+    response = HTTP.get(url)
+    normalizeReturnOrderBook(response)
   end
+
+  @doc false
+  def normalizeReturnOrderBook({:ok, 200, body}), do: {:ok, 200, normalizeReturnOrderBook(body)}
+  def normalizeReturnOrderBook(%{asks: asks, bids: bids} = body), do: Map.merge(body, %{asks: normalizeReturnOrderBook(asks), bids: normalizeReturnOrderBook(bids)})
+  def normalizeReturnOrderBook([price, amount]) when is_binary(price), do: [String.to_float(price), amount]
+  def normalizeReturnOrderBook(slot) when is_list(slot), do: Enum.map(slot, &(normalizeReturnOrderBook(&1)))
+  def normalizeReturnOrderBook(:error), do: :error
 end
--- test/krasukha/http/public_api_test.exs
+++ test/krasukha/http/public_api_test.exs
@@ -7,4 +7,11 @@ defmodule Krasukha.HTTP.PublicAPITest do
   test "uri/0 by default" do
     assert uri() == %URI{authority: "poloniex.com", fragment: nil, host: "poloniex.com", path: "/public", port: 443, query: nil, scheme: "https", userinfo: nil}
   end
+
+  test "normalizeReturnOrderBook/1" do
+    assert normalizeReturnOrderBook(["0.00003575", 663.12507632]) == [3.575e-5, 663.12507632]
+    actual = normalizeReturnOrderBook(%{asks: [["0.00003575", 663.12507632]], bids: [["0.00003554", 66.27588277]], isFrozen: "0", seq: 6260031})
+    expected = %{asks: [[3.575e-5, 663.12507632]], bids: [[3.554e-5, 66.27588277]], isFrozen: "0", seq: 6260031}
+    assert  actual == expected
+  end
 end
