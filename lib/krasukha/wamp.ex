a70d8efabe2702b2f5fe2c37dde17d983a9c4f0f
Zatvobor <a.zatvornitskiy@gmail.com>
2016-07-24 19:30:43 +0300

    Extract connection to Push API into WAMP module

--- lib/krasukha/wamp.ex
+++ lib/krasukha/wamp.ex
@@ -1,10 +1,43 @@
 defmodule Krasukha.WAMP do
   @moduledoc false
 
+  @credentials [realm: "realm1", timeout: 3000]
+  @doc false
+  def credentials, do: @credentials
+
   @uri URI.parse("wss://api.poloniex.com")
   @doc false
   def uri, do: @uri
 
   @doc false
   def url, do: uri |> to_string
+
+  @doc false
+  def connection do
+    Application.get_env(:krasukha, :wamp, %{subscriber: nil, options: nil})
+  end
+
+  @doc false
+  def connect(options \\ credentials) do
+    Spell.connect(url, options)
+  end
+
+  @doc false
+  def disconect(wamp_pid) do
+    Spell.close(wamp_pid)
+  end
+
+  @doc false
+  def connect!(options \\ credentials) do
+    {:ok, subscriber} = connect(options)
+    environment = %{subscriber: subscriber, options: options}
+    :ok = Application.put_env(:krasukha, :wamp, environment, [persistent: true])
+
+    {:ok , subscriber}
+  end
+
+  @doc false
+  def disconnect! do
+    disconect(connection().subscriber)
+  end
 end
--- test/krasukha/wamp_test.exs
+++ test/krasukha/wamp_test.exs
@@ -8,7 +8,28 @@ defmodule Krasukha.WAMPTest do
     assert uri() == %URI{authority: "api.poloniex.com", fragment: nil, host: "api.poloniex.com", path: nil, port: nil, query: nil, scheme: "wss", userinfo: nil}
   end
 
-  test "url/0" do
-    assert url() == "wss://api.poloniex.com"
+  test "url/0", do: assert url()
+  test "credentials/0", do: assert credentials()
+
+  test "connection by default" do
+    :ok = Application.delete_env(:krasukha, :wamp)
+    assert connection() == %{subscriber: nil, options: nil}
+  end
+
+  describe "Connection to Push API" do
+    @tag [external: true]
+    test "connect/0 and disconnect/1" do
+      {:ok, pid} = connect()
+      assert :ok == disconect(pid)
+    end
+
+    @tag [external: true]
+    test "connect!/0 and disconnect!/0" do
+      {:ok, _pid} = connect!()
+      %{subscriber: subscriber, options: options} = connection()
+      assert subscriber
+      assert options
+      assert :ok == disconnect!()
+    end
   end
 end
