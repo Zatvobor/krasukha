fba4ca852ea7520e07841918f63814a6c6025b1c
Zatvobor <a.zatvornitskiy@gmail.com>
2016-08-03 20:31:32 +0300

    Ability to start servers with initial requests

--- lib/krasukha.ex
+++ lib/krasukha.ex
@@ -21,6 +21,12 @@ defmodule Krasukha do
   end
 
   @doc false
+  def start_markets!(initial_requests \\ [:fetch_ticker, :subscribe_ticker]) do
+    {:ok, pid} = start_markets()
+    init(pid, initial_requests)
+  end
+
+  @doc false
   def start_market(currency_pair) do
     import Krasukha.MarketGen, only: [to_name: 2]
     spec = worker(Krasukha.MarketGen, [currency_pair], [id: to_name(currency_pair, :market), restart: :transient])
@@ -28,7 +34,19 @@ defmodule Krasukha do
   end
 
   @doc false
+  def start_market!(currency_pair, initial_requests \\ [{:fetch_order_book, [depth: 10]}, :subscribe]) do
+    {:ok, pid} = start_market(currency_pair)
+    init(pid, initial_requests)
+  end
+
+  @doc false
   def stop(_state) do
     Krasukha.WAMP.disconnect!()
+  end
+
+
+  defp init(pid, initial_requests) do
+    responses = Enum.map(initial_requests, fn(request) -> GenServer.call(pid, request) end)
+    {:ok, pid, responses}
   end
 end
