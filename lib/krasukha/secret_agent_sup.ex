d8898337bc309928bbc0a515756e6461f9eea8d4
Zatvobor <a.zatvornitskiy@gmail.com>
2016-11-30 13:34:32 +0200

    Add supervisors helpers for Krasukha.SecretAgent.Supervisor

--- lib/krasukha/secret_agent_sup.ex
+++ lib/krasukha/secret_agent_sup.ex
@@ -7,4 +7,17 @@ defmodule Krasukha.SecretAgent.Supervisor do
     result = for {^term, pid, :worker, [Krasukha.SecretAgent]} <- Supervisor.which_children(__MODULE__), do: pid
     List.first(result)
   end
+
+  @doc false
+  def which_children, do: Supervisor.which_children(__MODULE__)
+  @doc false
+  def count_children, do: Supervisor.count_children(__MODULE__)
+  @doc false
+  def get_childspec(id), do: :supervisor.get_childspec(__MODULE__, id)
+  @doc false
+  def terminate_child(id), do: Supervisor.terminate_child(__MODULE__, id)
+  @doc false
+  def restart_child(id), do: Supervisor.restart_child(__MODULE__, id)
+  @doc false
+  def delete_child(id), do: Supervisor.delete_child(__MODULE__, id)
 end
