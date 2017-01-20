d4476472d8f4c49dce288a956401384144dead79
Zatvobor <a.zatvornitskiy@gmail.com>
2017-01-20 11:37:35 +0200

    extract child context

--- test/krasukha/lending_routines_sup_test.exs
+++ test/krasukha/lending_routines_sup_test.exs
@@ -23,33 +23,33 @@ defmodule Krasukha.LendingRoutines.SupervisorTest do
     assert actual == pid
   end
 
-  test "terminate_children/0", %{agent: agent} do
-    {:ok, _} = Krasukha.start_lending_routine(agent, :available_balance_to_gap_position, %{currency: "X"})
-    {:ok, _} = Krasukha.start_lending_routine(agent, :available_balance_to_gap_position, %{currency: "Y"})
-    actual = terminate_children()
-    assert actual == [:ok, :ok]
-  end
+  describe "child context" do
+    setup %{agent: agent} do
+      for currency <- ["X", "Y"] do
+        {:ok, _} = Krasukha.start_lending_routine(agent, :available_balance_to_gap_position, %{currency: currency})
+      end
+    end
 
-  test "restart_children/0", %{agent: agent} do
-    {:ok, _} = Krasukha.start_lending_routine(agent, :available_balance_to_gap_position, %{currency: "X"})
-    {:ok, _} = Krasukha.start_lending_routine(agent, :available_balance_to_gap_position, %{currency: "Y"})
-    terminate_children()
-    actual = restart_children()
-    assert [{:ok, _}, {:ok, _}] = actual
-  end
+    test "terminate_children/0" do
+      actual = terminate_children()
+      assert actual == [:ok, :ok]
+    end
 
-  test "delete_children/0", %{agent: agent} do
-    {:ok, _} = Krasukha.start_lending_routine(agent, :available_balance_to_gap_position, %{currency: "X"})
-    {:ok, _} = Krasukha.start_lending_routine(agent, :available_balance_to_gap_position, %{currency: "Y"})
-    terminate_children()
-    actual = delete_children()
-    assert actual == [:ok, :ok]
-  end
+    test "restart_children/0" do
+      terminate_children()
+      actual = restart_children()
+      assert [{:ok, _}, {:ok, _}] = actual
+    end
 
-  test "get_childrenspec/0", %{agent: agent} do
-    {:ok, _} = Krasukha.start_lending_routine(agent, :available_balance_to_gap_position, %{currency: "X"})
-    {:ok, _} = Krasukha.start_lending_routine(agent, :available_balance_to_gap_position, %{currency: "Y"})
-    actual = get_childrenspec()
-    assert [{:ok, _}, {:ok, _}] = actual
+    test "delete_children/0" do
+      terminate_children()
+      actual = delete_children()
+      assert actual == [:ok, :ok]
+    end
+
+    test "get_childrenspec/0" do
+      actual = get_childrenspec()
+      assert [{:ok, _}, {:ok, _}] = actual
+    end
   end
 end
