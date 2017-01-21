bfc72cef73925780a74268ba7d19c8e0e3f0e515
Zatvobor <a.zatvornitskiy@gmail.com>
2017-01-21 12:39:05 +0200

    util functions for unix_time management

--- lib/krasukha/helpers/string.ex
+++ lib/krasukha/helpers/string.ex
@@ -18,4 +18,64 @@ defmodule Krasukha.Helpers.String do
     ArgumentError -> String.to_integer(value)
   end
   def to_float(value), do: value
+
+  @doc false
+  def to_erl_datetime(<<year::4-bytes, ?-, month::2-bytes, ?-, day::2-bytes, sep, hour::2-bytes, ?:, min::2-bytes, ?:, sec::2-bytes, rest::binary>>) when sep in [?\s, ?T] do
+    with {year, ""}       <- Integer.parse(year),
+         {month, ""}      <- Integer.parse(month),
+         {day, ""}        <- Integer.parse(day),
+         {hour, ""}       <- Integer.parse(hour),
+         {min, ""}        <- Integer.parse(min),
+         {sec, ""}        <- Integer.parse(sec),
+         {_microsec, rest} <- Calendar.ISO.parse_microsecond(rest),
+         {_offset, ""}    <- Calendar.ISO.parse_offset(rest) do
+      {{year, month, day}, {hour, min, sec}}
+    else
+      _ -> {:error, :invalid_format}
+    end
+  end
+
+  @doc false
+  def now_to_erl_datetime do
+    :calendar.now_to_universal_time(:erlang.timestamp())
+  end
+
+  @doc false
+  def from_erl_datetime({{year, month, day}, {hour, min, sec}}) do
+    "#{date_to_string(year, month, day)} #{time_to_string(hour, min, sec)}"
+  end
+
+  defp date_to_string(year, month, day) do
+    zero_pad(year, 4) <> "-" <> zero_pad(month, 2) <> "-" <> zero_pad(day, 2)
+  end
+
+  defp time_to_string(hour, minute, second) do
+    zero_pad(hour, 2) <> ":" <> zero_pad(minute, 2) <> ":" <> zero_pad(second, 2)
+  end
+
+  defp zero_pad(val, count) do
+    num = Integer.to_string(val)
+    :binary.copy("0", count - byte_size(num)) <> num
+  end
+
+
+  @doc false
+  @epoch :calendar.datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}})
+  def to_unix_time({{_year, _month, _day}, {_hour, _min, _sec}} = datetime) do
+    datetime
+      |> :calendar.datetime_to_gregorian_seconds
+      |> Kernel.-(@epoch)
+  end
+
+  @doc false
+  def from_unix_time(seconds) do
+    {days, time} = :calendar.seconds_to_daystime(seconds)
+    {years, month, day} = :calendar.gregorian_days_to_date(days)
+    {{1970 + years, month, day}, time}
+  end
+
+  @doc false
+  def now_to_unix_time() do
+    to_unix_time(now_to_erl_datetime())
+  end
 end
--- test/krasukha/helpers/string_test.exs
+++ test/krasukha/helpers/string_test.exs
@@ -8,4 +8,26 @@ defmodule Krasukha.Helpers.StringTest do
     assert to_tuple_with_floats(["0.00003575", 663.12507632]) == {3.575e-5, 663.12507632}
     assert to_tuple_with_floats(["10.0", 20]) == {10.0, 20}
   end
+
+  test "to_erl_datetime/1" do
+    assert to_erl_datetime("2017-01-20 10:39:35") == {{2017, 1, 20}, {10, 39, 35}}
+  end
+
+  test "from_erl_datetime/1" do
+    assert from_erl_datetime({{2017, 1, 20}, {10, 39, 35}}) == "2017-01-20 10:39:35"
+  end
+
+  test "to_unix_time/1" do
+    assert to_unix_time({{1970, 1, 1}, {0, 0, 0}}) == 0
+    assert to_unix_time({{2017, 1, 20}, {10, 39, 35}}) == 1484908775
+  end
+
+  test "from_unix_time/1" do
+    assert from_unix_time(0) == {{1970, 1, 1}, {0, 0, 0}}
+    assert from_unix_time(1484908775) == {{2017, 1, 20}, {10, 39, 35}}
+  end
+
+  test "now_to_unix_time/0" do
+    assert now_to_unix_time >= 1484923660
+  end
 end
