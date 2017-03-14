00e05a311090f35d8830aa88334a06d2c50ae299
Zatvobor <a.zatvornitskiy@gmail.com>
2017-03-14 11:21:53 +0200

    Bump to v0.1.0 pre-release

--- mix.exs
+++ mix.exs
@@ -1,14 +1,17 @@
 defmodule Krasukha.Mixfile do
   use Mix.Project
 
+  @origin "https://github.com/Zatvobor/krasukha"
+
   def project do
     [
       app: :krasukha,
+      description: "SDK for monitoring/trading/lending on Poloniex cryptocurrency exchange",
       version: "0.1.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      package: package,
+      package: package(),
       deps: deps()
     ]
   end
@@ -28,11 +31,9 @@ defmodule Krasukha.Mixfile do
 
   defp package do
     [
-      maintainers: ["Aleksey Zatvobor"], licenses: ["MIT"], links: %{"GitHub" => github, "Contributors" => contributors, "Issues" => issues}
+      maintainers: ["Aleksey Zatvobor"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @origin, "Contributors" => "#{@origin}/graphs/contributors", "Issues" => "#{@origin}/issues"}
     ]
   end
-
-  defp github, do: "https://github.com/Zatvobor/krasukha"
-  defp contributors, do: "https://github.com/Zatvobor/krasukha/graphs/contributors"
-  defp issues, do: "https://github.com/Zatvobor/krasukha/issues"
 end
