d3b872fd6f1b83c204fa4790b5c7c0a281d260d4
Zatvobor <a.zatvornitskiy@gmail.com>
2018-05-08 14:51:58 +0300

    spec: calls url/3 for public api

--- test/krasukha/http/public_api_test.exs
+++ test/krasukha/http/public_api_test.exs
@@ -1,3 +1,4 @@
+alias Krasukha.HTTP
 import Krasukha.HTTP.PublicAPI
 
 defmodule Krasukha.HTTP.PublicAPITest do
@@ -5,5 +6,9 @@ defmodule Krasukha.HTTP.PublicAPITest do
 
   test "uri/0 by default" do
     assert uri() == %URI{authority: "poloniex.com", fragment: nil, host: "poloniex.com", path: "/public", port: 443, query: nil, scheme: "https", userinfo: nil}
+  end
+
+  test "url/3" do
+    assert HTTP.url("returnTicker", [], uri) == "https://poloniex.com/public?command=returnTicker"
   end
 end
